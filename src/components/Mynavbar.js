f56d3e2dbef6814087c6f234f0c0b50384f69a53
krvne <lachyb742@gmail.com>
2021-06-19 21:31:32 +1000

    played with navigation js. failed. fixed visited.

--- src/components/Mynavbar.js
+++ src/components/Mynavbar.js
@@ -4,10 +4,18 @@ import '../css/mynavbar.css'
 const Mynavbar = () => {
     var logo = require('./images/logo.png')
     const currentPage = window.location.pathname
+    const hamburger = require('./images/hamburger.png')
+
+    // const ham = document.getElementById('hamburger')
+    // const navUl = document.getElementById('nav-ul')
+
+    // ham.addEventListener('click', () => {
+    //     navUl.classList.toggle('show')
+    // })
 
     return (
         <>
-            <header>
+            <header id='hide-mobile'>
                 <a href='/'><img class="logo" src={logo.default} width='60' height='60' alt='logo' /></a>
                 <nav>
                     <ul class="nav-links">
@@ -15,8 +23,21 @@ const Mynavbar = () => {
                         {(currentPage === '/about') ? <li><a className='active' href='/about'>About</a></li> : <li><a href='/about'>About</a></li>}
                     </ul>
                 </nav>
-                <a class="" href='/contact'><button>Contact</button></a>
+                <a href='/contact'><button>Contact</button></a>
             </header>
+            {/* <header id='show-mobile'>
+                <div className='nav'>
+                    <a href='/'><img class="logo" src={logo.default} width='60' height='60' alt='logo' /></a>
+                    <a className='right-padding' href='/contact'><button>Contact</button></a>
+                    <button id='hamburger' href='#'><img className='hamburger' src={hamburger.default} alt='' /></button>
+                </div>
+                <div>
+                    <ul class="nav-links" id='nav-ul'>
+                        {(currentPage === '/') ? <li><a className='active' href='/'>Home</a></li> : <li><a href='/'>Home</a></li>}
+                        {(currentPage === '/about') ? <li><a className='active' href='/about'>About</a></li> : <li><a href='/about'>About</a></li>}
+                    </ul>
+                </div>
+            </header> */}
         </>
 
     )
