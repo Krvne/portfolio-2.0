57fadbf8873cbb79285a4046b622e5de70daf1d3
krvne <lachyb742@gmail.com>
2021-06-21 20:53:40 +1000

    contact form workng. added basic field validation.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,38 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
 import '../../css/contact.css'
 
 const Contact = () => {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_owwdguk', 'template_xuzns8l', e.target, 'user_QHPAAN4RyoYjdmj4juAyU')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset()
+    }
+
     return (
         <>
             <main className='page-container'>
                 <div className='form-box-container'>
                     <div className='contact-form-box'>
-                        <form className='contact-form'>
+                        <form onSubmit={sendEmail} className='contact-form'>
                             <div className='form-group'>
-                                <label>Name</label>
-                                <input type='text' name='name' placeholder='John Doe' />
+                                <label>Name:</label>
+                                <input type='text' name='name' placeholder='John Doe' required />
                             </div>
                             <div className='form-group'>
-                                <label>Email</label>
-                                <input type='text' name='email' placeholder='john@doe.com' />
+                                <label>Email:</label>
+                                <input type='email' name='email' placeholder='john@doe.com' required />
                             </div>
                             <div className='form-group message'>
-                                <label>Message</label>
-                                <textarea className='message-field' name='message' wrap='soft' placeholder='What would you like to chat about?' />
+                                <label>Message:</label>
+                                <textarea className='message-field' name='message' wrap='soft' placeholder='What would you like to chat about?' required />
                             </div>
                             <div className='form-group submit'>
                                 <input className='submit-button' type='submit' name='submit' />
