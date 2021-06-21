53c2dfb6220d810803bea5ab4ecbbdddc710864e
krvne <lachyb742@gmail.com>
2021-06-21 17:18:40 +1000

    contact page design done

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import '../../css/contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            Contact page under construction
-        </div>
+        <>
+            <main className='page-container'>
+                <div className='box-container'>
+                    <div className='contact-form-box'>
+                        <form className='contact-form'>
+                            <div className='form-group'>
+                                <label>Name</label>
+                                <input type='text' name='name' placeholder='John Doe' />
+                            </div>
+                            <div className='form-group'>
+                                <label>Email</label>
+                                <input type='text' name='email' placeholder='john@doe.com' />
+                            </div>
+                            <div className='form-group message'>
+                                <label>Message</label>
+                                <textarea className='message-field' name='message' wrap='soft' placeholder='What would you like to chat about?' />
+                            </div>
+                            <div className='form-group submit'>
+                                <input className='submit-button' type='submit' name='submit' />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </main>
+        </>
     )
 }
 
